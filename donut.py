97424d915becd52c2f1bb78e7ed276cf713aec34
Aditya Sharma <adusharma2004@gmail.com>
2021-09-22 08:06:36 +0530

    yoyoyo

--- donut.py
+++ donut.py
@@ -7,10 +7,6 @@ def main():
 
     height=24
     width=80
-    #height=int(input("Enter Screen Height : "))
-    #width=int(input("Enter Screen Width : "))
-
-	# for clearing console (windows and unix systems)
     clear = "cls"
     if os.name == "posix":
         clear = "clear"
@@ -41,23 +37,14 @@ def main():
                 mess=1/(sini*cosj2*sinA+sinj*cosA+5)
                 t=sini*cosj2*cosA-sinj* sinA
 
-                # 40 is the left screen shift
                 x = int(40+30*mess*(cosi*cosj2*cosB-t*sinB))
-                # 12 is the down screen shift
                 y = int(11+15*mess*(cosi*cosj2*sinB +t*cosB))
-                # all are casted to int, ie floored
                 o = int(x+width*y)
-				# multiplying by 8 to bring in range 0-11 as 8*(sqrt(2))=11
-				# because we have 11 luminance characters
                 N = int(8*((sinj*sinA-sini*cosj*cosA)*cosB-sini*cosj*sinA-sinj*cosA-cosi *cosj*sinB))
-				# if x,y inside screen and previous z-buffer is < mess 
-				# i.e. when z[o] is 0 or the prev point is behind the new point
-				# so we change it to the point nearer to the eye/ above prev point 
                 if 0<y<height and 0<x<width and z[o] < mess:
                     z[o]=mess
                     screen[o]=".,-~:;=!*#$@"[N if N>0 else 0]
 
-        # prints
         os.system(clear)
         for index, char in enumerate(screen):
             if index % width == 0:
